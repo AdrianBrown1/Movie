73807ecdfcebb8874a4975ab8c8226e515d7cfc3
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-11 16:09:00 -0400

    feature: created landing-page container

---
+++ movie-app/src/containers/landing-page-container/landing-page-container.js
@@ -0,0 +1,25 @@
+import LandingPage from '../../screens/landing-page/landing-page';
+import { connect } from 'react-redux';
+// import { fetchWeather, resetWeatherState } from '../../actions/actions';
+// import weather from '../../reducers/weather';
+
+
+const mapStateToProps = (state) => {
+  return {
+   weather: state.weather,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+//   resetWeatherState: () => {
+//     dispatch(resetWeatherState());
+//   },
+//   fetchWeather: (params) => {
+//     dispatch(fetchWeather(params));
+//   },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  )(LandingPage);
