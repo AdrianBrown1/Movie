19268b6f5ca91b4a4db2feea7db516bba741234f
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-11 16:13:02 -0400

    refactor: refactored landing page component & styles

--- movie-app/src/screens/landing-page/landing-page.js
+++ movie-app/src/screens/landing-page/landing-page.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Header from '../../components/header/header'
-export default class Dashboard extends Component {
+
+export default class Landingpage extends Component {
 
   static propTypes = {
     // fetchWeather: PropTypes.func,
     // resetWeatherState: PropTypes.func,
     // weather: PropTypes.object,
   }
+  
+  _transitionToNextScreen = () => {
+    console.log('POOP');
+  }
 
   render() {
-
+    console.log(this.props);
+    const { history } = this.props;
+    console.log(history);
     return (
-      <div className="landing-page">
-        <Header/>
-        {/* <Tex fetchWeather={fetchWeather} weather={weather} resetWeatherState={resetWeatherState}/>
-        <Weather weather={weather}/> */}
+      <div className="landing-page__container">
+        <Header history={history}/>
+         <div className='landing-page'>
+          <span className='landing-page__title'>Welcome To FavMovies!</span>
+          <button className='landing-page__button' onClick={this._transitionToNextScreen}>Enter</button>
+         </div>
       </div>
     );
   }
