7d3572c0f0ff96dd3b0028ff7fa4863d31c5ef5d
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-14 23:09:28 -0400

    feature: created movie list page container

---
+++ movie-app/src/containers/movie-list-page-container/movie-list-page-container.js
@@ -0,0 +1,25 @@
+import MovieListPage from '../../screens/movie-list-page/movie-list-page';
+import { connect } from 'react-redux';
+import { fetchMovies, searchMovies } from '../../actions/actions';
+// import { fetchWeather, resetWeatherState } from '../../actions/actions';
+// import movies from '../../reducers/movies';
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchMovies: (params) => {
+    dispatch(fetchMovies(params));
+  },
+  searchMovies: (params) => {
+    dispatch(searchMovies(params));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  )(MovieListPage);
