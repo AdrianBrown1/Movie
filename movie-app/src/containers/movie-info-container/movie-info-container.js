a4886b0db10c0ae122cc9d7aa6ea117a5480a875
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-16 11:18:30 -0400

    feature: created movie info page

---
+++ movie-app/src/containers/movie-info-container/movie-info-container.js
@@ -0,0 +1,23 @@
+import MovieInfo from '../../screens/movie-info-page/movie-info';
+import { connect } from 'react-redux';
+import { fetchMovies, searchMovies } from '../../actions/actions';
+// import { fetchWeather, resetWeatherState } from '../../actions/actions';
+// import movies from '../../reducers/movies';
+
+const mapStateToProps = (state) => {
+  return state.movies;
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchMovies: (params) => {
+    dispatch(fetchMovies(params));
+  },
+  searchMovies: (params) => {
+    dispatch(searchMovies(params));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  )(MovieInfo);
---
+++ movie-app/src/screens/movie-info-page/movie-info.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../../components/header/header'
+import MovieList from '../../components/movie-list/movie-list';
+
+export default class MovieListPage extends Component {
+
+  static propTypes = {
+    fetchMovies: PropTypes.func,
+  }
+
+
+//   _inputSubmit = (e) => {
+//     //get value from search form & search for movie!
+//     // once movies come back pass movies to movies to movie list. 
+//     const { searchMovies } = this.props;
+//     const movieTitleToSearch = this.title.value.replace(/\ /g, "+");
+//     searchMovies(movieTitleToSearch);
+//     e.preventDefault();
+//     e.target.reset();
+//   }
+
+  render() {
+    const { history } = this.props;
+    
+    return (
+      <div className="movie-list__container">
+        <Header history={history}/>
+         <div>Hello world</div>
+      </div>
+    );
+  }
+}
